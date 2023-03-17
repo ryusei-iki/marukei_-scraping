8737ac86695d920c347d18ca31b7fba54e8f8d9b
ryusei-iki <0516.iki@gmail.com>
2023-03-17 10:48:11 +0900

    皿カテゴリだけのurlを全部取得

--- scraping_image.py
+++ scraping_image.py
@@ -1,17 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-page_url = 'https://www.arita-marukei.com/?mode=srh&cid=&keyword='
+
+page_url_base = 'https://www.arita-marukei.com/?mode=cate&cbid=2482976&csid=0'
+page_num = 52
 img_url_base = 'https://www.arita-marukei.com/'
-r = requests.get(page_url)
-soup = BeautifulSoup(r.text)
 urls = []
-contents = soup.find('ul', class_='row-list row-list--middle')
-
-
-a_elements = contents.find_all('a')
-for a_element in a_elements:
-    # print('herf:{}'.format(a_element.get('href')))
-    urls.append(img_url_base + a_element.get('href'))
+for page in range(page_num):
+    if (page == 0):
+        r = requests.get(page_url_base)
+    else:
+        r = requests.get(page_url_base + '&page=' + str(page))
+    soup = BeautifulSoup(r.text)
+    contents = soup.find('ul', class_='row-list row-list--middle')
+    a_elements = contents.find_all('a')
+    for a_element in a_elements:
+        # print('herf:{}'.format(a_element.get('href')))
+        urls.append(img_url_base + a_element.get('href'))
 
 print(urls)
