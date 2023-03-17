b6e823195b99b7ed990ed2be066588e037ea725a
ryusei-iki <0516.iki@gmail.com>
2023-03-17 10:51:46 +0900

    __main__にした

--- scraping_image.py
+++ scraping_image.py
@@ -2,20 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 
 
-page_url_base = 'https://www.arita-marukei.com/?mode=cate&cbid=2482976&csid=0'
-page_num = 52
-img_url_base = 'https://www.arita-marukei.com/'
-urls = []
-for page in range(page_num):
-    if (page == 0):
-        r = requests.get(page_url_base)
-    else:
-        r = requests.get(page_url_base + '&page=' + str(page))
-    soup = BeautifulSoup(r.text)
-    contents = soup.find('ul', class_='row-list row-list--middle')
-    a_elements = contents.find_all('a')
-    for a_element in a_elements:
-        # print('herf:{}'.format(a_element.get('href')))
-        urls.append(img_url_base + a_element.get('href'))
+if __name__ == '__main__':
+    page_url_base = 'https://www.arita-marukei.com/?mode=cate&cbid=2482976&csid=0'
+    page_num = 52
+    img_url_base = 'https://www.arita-marukei.com/'
+    urls = []
 
-print(urls)
+    for page in range(page_num):
+        if (page == 0):
+            r = requests.get(page_url_base)
+        else:
+            r = requests.get(page_url_base + '&page=' + str(page))
+        soup = BeautifulSoup(r.text)
+        contents = soup.find('ul', class_='row-list row-list--middle')
+        a_elements = contents.find_all('a')
+        for a_element in a_elements:
+            # print('herf:{}'.format(a_element.get('href')))
+            urls.append(img_url_base + a_element.get('href'))
+
+    print(urls)
