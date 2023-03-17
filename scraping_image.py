cbe9aabde53f2c4d4d87990acc88c1f920ff5e14
ryusei-iki <0516.iki@gmail.com>
2023-03-17 10:39:20 +0900

    1メージ目のurl取得

--- scraping_image.py
+++ scraping_image.py
@@ -5,11 +5,13 @@ page_url = 'https://www.arita-marukei.com/?mode=srh&cid=&keyword='
 img_url_base = 'https://www.arita-marukei.com/'
 r = requests.get(page_url)
 soup = BeautifulSoup(r.text)
-
+urls = []
 contents = soup.find('ul', class_='row-list row-list--middle')
 
 
-contents_a = contents.a
+a_elements = contents.find_all('a')
+for a_element in a_elements:
+    # print('herf:{}'.format(a_element.get('href')))
+    urls.append(img_url_base + a_element.get('href'))
 
-print('herf:{}'.format(contents_a.get('href')))
-
+print(urls)
