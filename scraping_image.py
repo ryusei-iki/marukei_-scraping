c33e25be4ce3b500c4271bd54ce9e05052a34a85
ryusei-iki <103087980+ryusei-iki@users.noreply.github.com>
2023-03-17 13:14:59 +0900

    Image scraping (#2)
    * 画像のurl取得
    
    * 画像を取得
    
    * 画像をすべて取得

--- scraping_image.py
+++ scraping_image.py
@@ -2,14 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 
 
+def download_image(url, file_path):
+    r = requests.get(url, stream=True)
+
+    if r.status_code == 200:
+        with open(file_path, "wb") as f:
+            f.write(r.content)
+
+
 if __name__ == '__main__':
     page_url_base = 'https://www.arita-marukei.com/?mode=cate&cbid=2482976&csid=0'
     page_num = 52
     img_url_base = 'https://www.arita-marukei.com/'
     urls = []
 
-    for page in range(page_num):
-        if (page == 0):
+    for page in range(1, page_num + 1):
+        if (page == 1):
             r = requests.get(page_url_base)
         else:
             r = requests.get(page_url_base + '&page=' + str(page))
@@ -19,5 +27,19 @@ if __name__ == '__main__':
         for a_element in a_elements:
             # print('herf:{}'.format(a_element.get('href')))
             urls.append(img_url_base + a_element.get('href'))
+        print('page:{}={}'.format(page, len(a_elements)))
 
-    print(urls)
+    for i, url in enumerate(urls):
+        print(i)
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text)
+        contents = soup.find('ul', class_='product__gallery')
+        img_elements = contents.find_all('img')
+        img_urls = []
+        for img_elements in img_elements:
+            img_urls.append(img_elements.get('src'))
+        # print(*img_urls, sep='\n')
+        for i, img_url in enumerate(img_urls):
+            file_name = '{}_{}.png'.format(url[url.find('=') + 1:], i)
+            image_path = 'images_sara/{}'.format(file_name)
+            download_image(url=img_url, file_path=image_path)
