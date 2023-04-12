d97292667570c9313a66868901a5f546f9e35b6f
ryusei-iki <0516.iki@gmail.com>
2023-04-12 20:20:00 +0900

    フォルダの自動作成と効率化

--- scraping_image.py
+++ scraping_image.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import os
 
 
 def download_image(url, file_path):
@@ -29,19 +30,22 @@ if __name__ == '__main__':
             # print('herf:{}'.format(a_element.get('href')))
             urls.append(img_url_base + a_element.get('href'))
         print('page:{}={}'.format(page, len(a_elements)))
-
+    os.makedirs('images_sara', exist_ok=True)
     # 取得したリンクから画像を取得
     for i, url in enumerate(urls):
-        print(i)
+        print('{}/{}'.format(i, len(urls)))
         r = requests.get(url)
         soup = BeautifulSoup(r.text)
         contents = soup.find('ul', class_='product__gallery')
         img_elements = contents.find_all('img')
         img_urls = []
-        for img_elements in img_elements:
-            img_urls.append(img_elements.get('src'))
-        # print(*img_urls, sep='\n')
-        for i, img_url in enumerate(img_urls):
-            file_name = '{}_{}.png'.format(url[url.find('=') + 1:], i)
+        for j, img_elements in enumerate(img_elements):
+            img_url = img_elements.get('src')
+            file_name = '{}_{}.png'.format(i, j)
             image_path = 'images_sara/{}'.format(file_name)
             download_image(url=img_url, file_path=image_path)
+        # print(*img_urls, sep='\n')
+        # for j, img_url in enumerate(img_urls):
+        #     file_name = '{}_{}.png'.format(i, j)
+        #     image_path = 'images_sara/{}'.format(file_name)
+        #     download_image(url=img_url, file_path=image_path)
